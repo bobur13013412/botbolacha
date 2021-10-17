781ca35c0ac0c09b26feca6e394abcbe410653ca
bobur13013412 <53964721+bobur13013412@users.noreply.github.com>
2021-10-17 19:55:19 +0300

    sal

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-from translate import translate
-import telebot
+import os
+import time
 
-TOKEN = "1966517117:AAHoK_yi4S3hxh2olYtLFgVCt5toGo9NWrw"
-tarjimonbot  = telebot.TeleBot(token=TOKEN)
+while(True):
+    os.system("py a.py")
+    time.sleep(60*30)
+# from translate import translate
+# import telebot
 
-# \start komandasi uchun mas'ul funksiya
-@tarjimonbot.message_handler(commands=['start'])
-def salom(message):    
-    xabar = "Assalom alaykum, tarjimon botiga xush kelbisiz."
-    xabar += '\nMatningizni yuboring.'
-    tarjimonbot.reply_to(message, xabar)
+# TOKEN = "1966517117:AAHoK_yi4S3hxh2olYtLFgVCt5toGo9NWrw"
+# tarjimonbot  = telebot.TeleBot(token=TOKEN)
+
+# # \start komandasi uchun mas'ul funksiya
+# @tarjimonbot.message_handler(commands=['start'])
+# def salom(message):    
+#     xabar = "Assalom alaykum, tarjimon botiga xush kelbisiz."
+#     xabar += '\nMatningizni yuboring.'
+#     tarjimonbot.reply_to(message, xabar)
     
 
-# matnlar uchun mas'ul funksiya
-@tarjimonbot.message_handler(func=lambda msg: msg.text is not None)
-def tarjima(message):
-    msg = message.text
-    tarjimonbot.reply_to(message, translate(msg))
+# # matnlar uchun mas'ul funksiya
+# @tarjimonbot.message_handler(func=lambda msg: msg.text is not None)
+# def tarjima(message):
+#     msg = message.text
+#     tarjimonbot.reply_to(message, translate(msg))
     
 
-tarjimonbot.polling()
+# tarjimonbot.polling()
